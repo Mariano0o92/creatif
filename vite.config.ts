04c7778b1b9fac51b0fb80e9486e5c8223a5c60a
Mariano0o92 <mariuszn13@gmail.com>
2025-02-14 14:39:01 +0100

    Another try to fix dark theme issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+const ASSET_URL = process.env.ASSET_URL || '';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: '/'
+	base: `${ASSET_URL}`
 })
